2ac81dcb65893ba115a466f7842a43c11785a7ce
ethan <ethan.gonzalez00774@gmail.com>
2022-12-15 10:19:27 -0800

    servelts with counter exercise complete

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -9,12 +9,18 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
+private int counter = 0;
+
+//public void init () {
+//    counter = 0;
+//}
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        counter++;
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>Hello, World!</h1>" + "<h2>The count is :" +  counter + "</h2>");
     }
 
 }
---
+++ src/main/webapp/counter.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: luciddreamz
+  Date: 12/15/22
+  Time: 09:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%  counter += 1; %>
+
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<>
+
+</body>
+</html>
